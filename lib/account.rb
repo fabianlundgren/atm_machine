e30a1004cc49f395c3fabae8cf802f5d3075bb3f
fabianlundgren <fabbe.lundgren@gmail.com>
2017-02-09 10:56:03 +0100

    Fixed the last errors I had and tidied up the code a bit

---
+++ lib/account.rb
@@ -0,0 +1,36 @@
+  class Account
+    attr_accessor :exp_date, :pin_code, :balance, :account_status, :owner
+
+    STANDARD_VALIDITY_YRS = 5
+
+    def initialize (attrs = {})
+      @exp_date = set_expire_date
+      @pin_code = generate_pin
+      @balance = 0
+      @account_status = :active
+      set_owner(attrs[:owner])
+  end
+
+  def deactivate
+    @account_status = :deactivated
+  end
+
+  private
+
+  def generate_pin
+    rand(1000..9999)
+  end
+
+  def set_owner(obj)
+    obj == nil ? missing_owner : @owner = obj
+  end
+
+  def missing_owner
+    raise 'An Account owner is required'
+  end
+
+  def set_expire_date
+    Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
+  end
+
+end
